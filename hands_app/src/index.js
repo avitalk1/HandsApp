f2026069d0f732f2b123bc9a06cc366da9e14911
chen gutman <chengutman0@gmail.com>
2020-02-25 22:46:01 +0200

    no theme

--- hands_app/src/index.js
+++ hands_app/src/index.js
@@ -6,25 +6,16 @@ import ReactRouter from './router/router';
 import * as serviceWorker from './serviceWorker';
 import logo from './images/logo.png';
 import 'typeface-roboto'
-// import Posts from './pages/Posts'
-// import AdminInbox from './pages/AdminInbox'
-// import RequestForm from './Components/RequestForm'
-// import CreatePost from './Components/Post/CreatePostForm'
-// import PostPhotos from './Components/Post/PostPhotos'
-// import CreatePostPage from './pages/CreatePostPage'
-// import RequestPage from './pages/Request'
+
 
 ReactDOM.render(
     <Router>
-        <img src={logo} className="logo" alt="logo" />
+        <div className="header">
+            <img src={logo} className="logo" alt="logo" />
+        </div>
         <ReactRouter />
-    </Router>,
-    document.getElementById('root')
+    </Router>
+    , document.getElementById('root')
 )
-//ReactDOM.render(<CreatePostPage />, document.getElementById('root'));
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
