7a19177c1490dd4c93b5df3a227dfb158d640ec8
avital <avital@pmax.co>
2020-02-26 18:04:31 +0200

    commit

--- hands_app/src/router/router.js
+++ hands_app/src/router/router.js
@@ -1,5 +1,5 @@
 
-import React, {useStae, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import { Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import MobileLandingPage from "../Mobile/MobileLandingPage";
